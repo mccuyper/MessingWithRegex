d69386dd13fe32f799e101d56076e7c4374971cb
Alexandr Kononuk <49821742+mccuyper@users.noreply.github.com>
2021-01-05 15:51:51 +0200

    Add files via upload

---
+++ findWebURL.py
@@ -0,0 +1,21 @@
+import pyperclip, re
+
+protocolRegex = re.compile(r'''
+    https?://           
+    (?:w{3}\.)?         
+    [\_?\-?a-zA-Z0-9\.$]+                      
+    [a-zA-Z]      
+    ''', re.VERBOSE)
+
+text = str(pyperclip.paste()) # variable contents data clipboard and converting
+matches = [] # empty list for matches
+
+for website in protocolRegex.findall(text): # finding website from text
+    matches.append(website) # if find regex append to matches list
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(map(str, matches))) # copying result to clipboard after adding newline after each match
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No website found')
